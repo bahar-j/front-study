ccf5bab91717b8d5126e134f48fa5f270e762ba3
June <joonie811@naver.com>
2020-08-17 15:26:46 +0900

    Add streaming site

--- JavaScript,jQuery/Aug_week2/13.streamingSite2/js/script.js
+++ JavaScript,jQuery/Aug_week2/13.streamingSite2/js/script.js
@@ -3,19 +3,29 @@ const playlist = document.querySelectorAll(".playlist");
 showPlaylist();
 //scroll 할 때마다 확인해서 보여줌
 $(window).on('scroll', showPlaylist);
+$(window).on('scroll', showButton);
 
 function showPlaylist() {   
     playlist.forEach( box => { 
         // element의 visible height 구하기
-        var scrollTop = $(this).scrollTop(),
-            scrollBot = scrollTop + $(this).height(),
+        var scrollTop = $(window).scrollTop(),
+            scrollBot = scrollTop + $(window).height(),
             elTop = $(box).offset().top,
             elBottom = elTop + $(box).outerHeight(),
             visibleTop = elTop < scrollTop ? scrollTop : elTop,
             visibleBottom = elBottom > scrollBot ? scrollBot : elBottom;
         // visible height가 실제 height의 절반 이상이면 투명도 1로
         if ((visibleBottom - visibleTop) >= (elBottom - elTop)/2){
-            $(box).animate({opacity: '1'}, 1000);
+            $(box).animate({opacity: 1}, 1000);
         }
     });
+}
+
+function showButton() {
+    if($(this).scrollTop() + $(this).height() === $(document).height()) {
+        $('.to-top-btn').fadeIn();
+    }
+    else{
+        $('.to-top-btn').fadeOut();
+    }
 }
